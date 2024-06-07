59307a2c3cfd013166257332994e172c2ca309ed
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-07 19:45:33 +0530

    shell script concepts

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -6,5 +6,5 @@ DOCUMENTARY=("The Elephant" "The Cat" "The Cheetah")
 
 echo "First Documentary is: ${DOCUMENTARY[0]}"
 echo "Second Documentary is: ${DOCUMENTARY[1]}"
-echo "All Documentaries is: ${DOCUMENTARY[#]}"
+echo "All Documentaries is: ${DOCUMENTARY[@]}"
 
