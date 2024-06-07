2a35b13c53ee2411bdf0f11ae2473c2cc25627ae
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-07 11:36:28 +0530

    shell script concepts

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#declaring variables
+PERSON1=$1
+PERSON2=$2
+
+#referring variables
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, i am fine, how are you?"
+echo "$PERSON1:: I am fine too, how is your work?"
+echo "$PERSON2:: not bad, i am thinking to upgrade to DevOps"
