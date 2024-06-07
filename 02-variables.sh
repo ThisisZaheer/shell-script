9ce833bdc27aa8bd88cb10fd04eb4726526d6989
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-07 11:26:45 +0530

    shell script concepts

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Zaheer:: Hello Shaik, how are you?"
+echo "Shaik:: Hi Zaheer, i am fine, how are you?"
+echo "Zaheer:: I am fine too, how is your work?"
+echo "Shaik:: not bad, i am thinking to upgrade to DevOps"
