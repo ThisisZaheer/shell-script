4cbb88e4e4168b82270080861232e7c8db831459
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-08 22:30:45 +0530

    shell-script

--- 111-sh.sh
+++ 111-sh.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "Hi,Devops"
+
+echo "zaheer is good in cricket"
+echo "shaik is good in football"
+echo "zaheer is batsman"
+echo "shaik is goalkeeper"
+echo "zaheer batting strike rate is 175"
+echo "shaik keeping is just beyond"
