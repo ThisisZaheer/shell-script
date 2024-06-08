4233d5f5f19b907ee1c29b080bc37aa97016812f
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-08 22:40:04 +0530

    shell-script

--- 111-sh.sh
+++ 111-sh.sh
@@ -11,3 +11,13 @@ echo "$player1 is batsman"
 echo "$player2 is goalkeeper"
 echo "$player1 batting strike rate is 175"
 echo "$player2 keeping is just beyond"
+
+echo "What is your bank atm card name --> "
+
+read SBI
+
+echo "what is your atm pin -->"
+
+read PIN
+
+echo "Card name --> $SBI, Atm pin --> $PIN"
