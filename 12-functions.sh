27916dbc16a3b4a491dfe02242b37cd859c0c77d
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-15 12:02:14 +0530

    script

--- 12-functions.sh
+++ 12-functions.sh
@@ -2,10 +2,21 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+    echo "Exit Status: $1"
+    echo "Installing: $2"
+}
+
 if [ $USERID -ne 0 ]
 then 
     echo "Please run this Script with root access"
-    
+    exit 1 # manually exit if error comes
 else
     echo "You are a Super User"
 fi
+
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
+
+dnf install git -y
+VALIDATE $? "Installing git"
