6c80349dd4894c00d9dbd8571b65bdab7061e124
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-07-03 08:54:35 +0530

    Shell-Script

--- 16-current-script.sh
+++ 16-current-script.sh
@@ -6,6 +6,6 @@ echo "Before Calling the other-script:$COURSE"
 echo "Process-ID of Current Shell-Script:$$"
 
 #./17-other-script.sh
-source./17-other-script.sh
+source ./17-other-script.sh
 
 echo "After Calling the other-script:$COURSE"
