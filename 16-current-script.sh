8b330005388d08b7eec68f706e9a5957a7dea874
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-07-03 08:43:28 +0530

    Shell-Script

--- 16-current-script.sh
+++ 16-current-script.sh
@@ -2,9 +2,9 @@
 
 COURSE="Devops from Current-Script" 
 
-echo "Before Calling the other-script, course:$COURSE"
+echo "Before Calling the other-script:$COURSE"
 echo "Process-ID of Current Shell-Script:$$"
 
 ./17-other-script.sh
 
-echo "After Calling the other-script, course:$COURSE"
+echo "After Calling the other-script:$COURSE"
