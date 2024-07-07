cc85619ee7764969f563afa2d9f19286b4285aeb
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-07-07 19:58:25 +0530

    Shell-Script

--- 14-loops.sh
+++ 14-loops.sh
@@ -5,4 +5,7 @@ do
    echo $i
 done
 
-
+for i in {990..1025}
+do
+   echo $i
+done
--- 17-other-script.sh
+++ 17-other-script.sh
@@ -4,3 +4,5 @@ COURSE="DevOps from Other-Script"
 
 echo "Variable Value from Other-Script:$COURSE"
 echo "Process-ID of other-script:$$"
+
+
